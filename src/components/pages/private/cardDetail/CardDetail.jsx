d7b7cd2c242f8514f1aae8a0a0a41eca724a030e
LautaroDellolio <lautarodellolio@gmail.com>
2023-09-24 20:40:35 -0300

    TrabajandoLaCardDetail

--- src/components/pages/private/cardDetail/CardDetail.jsx
+++ src/components/pages/private/cardDetail/CardDetail.jsx
@@ -2,11 +2,46 @@ import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom'
 import axios from 'axios';
 import { DataContext } from '../../../utils/GlobalContexts';
+import "./CardDetail.css"
+
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { Box } from '@mui/material';
+import { CardTravelRounded } from '@mui/icons-material';
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
 
 const CardDetail = () => {
-    const { state} = useContext(DataContext)
-    const [ movie, setMovie]  = useState({})
+    const { state } = useContext(DataContext)
+    const [movie, setMovie] = useState({})
     const { id } = useParams()
+    const [expanded, setExpanded] = useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
 
     useEffect(() => {
         const apiUrl = `https://api.themoviedb.org/3/movie/${id}?language=en-US'`;
@@ -26,17 +61,34 @@ const CardDetail = () => {
             });
     }, []);
 
+    console.log(movie);
     return (
-        <div  style={{ 
+        <div style={{
             background: state.theme.background,
             color: state.theme.color,
-            height:"90vh"
-            }}>
+            height: "90vh"
+        }}>
             {movie ? (
-                <div>
-                    <h2>{movie.title}</h2>
-                    <h3>en construcción...</h3>
-                </div>
+                <Box className="box-container"
+                    sx={{
+                        backgroundImage: `url(https://image.tmdb.org/t/p/w500/${movie.backdrop_path})`, // URL de imagen dinámica
+                        backgroundSize: 'cover',
+                        width: '100%',
+                        height: '100%',
+                    }}>
+
+                    <Box sx={{
+                        backgroundColor: "black",
+                        color: 'white',
+                        height: "100%",
+                        width: "100%",
+                        opacity: "0.8",
+                        
+                    }}>
+                        <img className='img-poster' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt="img-poster" />
+                        {movie.title}
+                    </Box>
+                </Box>
             ) : (
                 <p>Cargando...</p>
             )}
